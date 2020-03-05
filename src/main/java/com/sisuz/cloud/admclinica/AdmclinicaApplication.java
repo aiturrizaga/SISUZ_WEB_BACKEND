ef1b55bd21f489f92b2a8a6c13548b92ea2815c7
aiturrizaga <aiturrizaga.vargas@gmail.com>
2020-03-04 19:37:03 -0500

    initial project

---
+++ src/main/java/com/sisuz/cloud/admclinica/AdmclinicaApplication.java
@@ -0,0 +1,13 @@
+package com.sisuz.cloud.admclinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmclinicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmclinicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sisuz/cloud/admclinica/AdmclinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisuz.cloud.admclinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmclinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
